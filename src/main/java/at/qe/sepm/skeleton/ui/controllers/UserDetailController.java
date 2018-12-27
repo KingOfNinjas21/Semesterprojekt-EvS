3c4631e156b753144dab1c533cf79e3d8d53f32e
Salih Candir <salih.candir@student.uibk.ac.at>
2018-12-27 22:08:18 +0100

    admin can add new Users

--- src/main/java/at/qe/sepm/skeleton/ui/controllers/UserDetailController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/UserDetailController.java
@@ -2,6 +2,11 @@ package at.qe.sepm.skeleton.ui.controllers;
 
 import at.qe.sepm.skeleton.model.User;
 import at.qe.sepm.skeleton.services.UserService;
+import at.qe.sepm.skeleton.utils.PasswordGenerator;
+import at.qe.sepm.skeleton.utils.UserRolesView;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -19,11 +24,21 @@ public class UserDetailController {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private UserRolesView userRoleView;
 
     /**
      * Attribute to cache the currently displayed user
      */
     private User user;
+    private User newUser;
+    
+    
+    @PostConstruct
+    private void init() {
+    	newUser = new User();
+    }
 
     /**
      * Sets the currently displayed user and reloads it form db. This user is
@@ -47,7 +62,15 @@ public class UserDetailController {
         return user;
     }
 
-    /**
+    public User getNewUser() {
+		return newUser;
+	}
+
+	public void setNewUser(User newUser) {
+		this.newUser = newUser;
+	}
+
+	/**
      * Action to force a reload of the currently displayed user.
      */
     public void doReloadUser() {
@@ -67,6 +90,28 @@ public class UserDetailController {
     public void doDeleteUser() {
         this.userService.deleteUser(user);
         user = null;
+    }
+    
+    public void doAddUser() {
+    	
+    	if ((newUser.getUsername() == null) ||
+    			(newUser.getUsername().length() <= 3)) {
+    		//TODO: return message
+    		return;
+    	}
+    	
+    	
+    	
+    	// TODO: send password via email
+    	String pass = PasswordGenerator.getRandomPassword(5);
+    	String hashedPass = PasswordGenerator.hashPassword(pass);
+    	
+    	newUser.setPassword(hashedPass);
+    	newUser.setEnabled(true);
+    	newUser.setRoles(userRoleView.getSelectedSet());
+    	
+    	userService.saveUser(newUser);
+    	newUser = new User();
     }
 
 }
---
+++ src/main/java/at/qe/sepm/skeleton/utils/UserRolesView.java
@@ -0,0 +1,41 @@
+package at.qe.sepm.skeleton.utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+
+import at.qe.sepm.skeleton.model.UserRole;
+
+@ManagedBean(name = "userRolesView")
+@Scope("view")
+@Controller
+public class UserRolesView {
+    private UserRole[] selected;
+
+
+	public UserRole[] getRoles() {
+		return UserRole.values();
+	}
+
+
+	public UserRole[] getSelected() {
+		return selected;
+	}
+	
+	public Set<UserRole> getSelectedSet() {
+		return new HashSet<UserRole>(Arrays.asList(selected));
+	}
+
+
+	public void setSelected(UserRole[] selectedRoles) {
+		this.selected = selectedRoles;
+	}
+
+
+}
