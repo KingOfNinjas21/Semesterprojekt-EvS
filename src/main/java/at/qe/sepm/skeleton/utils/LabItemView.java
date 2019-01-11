3c59d4713a9550e1f203aea699b75c78a1d867e9
schno <schno@DESKTOP-8MR42M0>
2019-01-11 14:50:58 +0100

    lab

---
+++ src/main/java/at/qe/sepm/skeleton/utils/LabItemView.java
@@ -0,0 +1,57 @@
+package at.qe.sepm.skeleton.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import at.qe.sepm.skeleton.model.StockItem;
+import at.qe.sepm.skeleton.repositories.StockRepository;
+
+/**
+ * A lab item view. Loads the items from {@code LabItemRepository}.
+ * 
+ * @author Candir Salih
+ */
+@ManagedBean(name = "labItemView")
+@Scope("view")
+@Controller
+public class LabItemView
+{
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	private List<StockItem> items;
+	private List<StockItem> selectedItems;
+
+	@PostConstruct
+	public void init()
+	{
+		items = new ArrayList<StockItem>();
+		selectedItems = new ArrayList<StockItem>();
+
+		items.addAll(stockRepository.findAll());
+	}
+
+	public List<StockItem> getItems()
+	{
+		return items;
+	}
+
+	public List<StockItem> getSelectedItems()
+	{
+		return selectedItems;
+	}
+
+	public void setSelectedItems(List<StockItem> selectedLabItems)
+	{
+		this.selectedItems = selectedLabItems;
+	}
+
+}
