80d32ca7c4198428eac2c822591d359594406c2f
schno <schno@DESKTOP-8MR42M0>
2019-01-27 20:34:30 +0100

    email with password

--- src/main/java/at/qe/sepm/skeleton/services/EmailService.java
+++ src/main/java/at/qe/sepm/skeleton/services/EmailService.java
@@ -1,156 +1,166 @@
 package at.qe.sepm.skeleton.services;
 
-import at.qe.sepm.skeleton.model.Reservation;
-
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import at.qe.sepm.skeleton.model.StockItem;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
 import java.util.Collection;
 import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import at.qe.sepm.skeleton.model.Reservation;
+import at.qe.sepm.skeleton.model.StockItem;
+
 @Service
 @Scope("application")
-public class EmailService {
+public class EmailService
+{
 
-    private static final String senderEmail = "sepmsender@gmail.com";
-    private static final String senderPassword = "sender123";
+	private static final String senderEmail = "sepmsender@gmail.com";
+	private static final String senderPassword = "sender123";
 
-    @Autowired
-    private AuditLogService auditLogService;
+	@Autowired
+	private AuditLogService auditLogService;
 
-    @Autowired
-    private ReservationService reservationService;
+	@Autowired
+	private ReservationService reservationService;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    private void prepareEmailMessage(MimeMessage message, String to, String title, String html)
-            throws MessagingException {
-        message.setContent(html, "text/html; charset=utf-8");
-        message.setFrom(new InternetAddress(senderEmail));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-        message.setSubject(title);
-    }
+	private void prepareEmailMessage(MimeMessage message, String to, String title, String html)
+			throws MessagingException
+	{
+		message.setContent(html, "text/html; charset=utf-8");
+		message.setFrom(new InternetAddress(senderEmail));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		message.setSubject(title);
+	}
 
-    private Session createSession() {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+	private Session createSession()
+	{
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderEmail, senderPassword);
-            }
-        });
-        return session;
-    }
+		Session session = Session.getInstance(props, new javax.mail.Authenticator()
+		{
+			protected PasswordAuthentication getPasswordAuthentication()
+			{
+				return new PasswordAuthentication(senderEmail, senderPassword);
+			}
+		});
+		return session;
+	}
 
-    //Notifies the user about his reservation
-    public void reservationCreatedNotification(Reservation reservation, Collection<StockItem> selectedItems) throws MessagingException {
-        String stockItemsString ="";
-        for(StockItem item: selectedItems){
-            stockItemsString = item.getLabItem().getItemName() + ", ";
-        }
+	// Notifies the user about his reservation
+	public void reservationCreatedNotification(Reservation reservation, Collection<StockItem> selectedItems)
+			throws MessagingException
+	{
+		String stockItemsString = "";
+		for (StockItem item : selectedItems)
+		{
+			stockItemsString = item.getLabItem().getItemName() + ", ";
+		}
 
-        String title = "Reservation created!";
-        String html =
-                "<h1>We created a reservation for you</h1>" +
-                        "You reserved "+ selectedItems.size() + " items:<br>" +
-                        stockItemsString +
-                "<br>Please bring it back before the reservation expires on: " + reservation.getReturnableDate() +
-                "<br><br>Kind regards,<br> Group 4";
+		String title = "Reservation created!";
+		String html = "<h1>We created a reservation for you</h1>" + "You reserved " + selectedItems.size()
+				+ " items:<br>" + stockItemsString + "<br>Please bring it back before the reservation expires on: "
+				+ reservation.getReturnableDate() + "<br><br>Kind regards,<br> Group 4";
 
-        if (reservation.getUser().getEmail() == null){
-            auditLogService.reservationUserEmailInvalid(reservation);
-            return;
-        }
-        System.out.println("Sending email to " + reservation.getUser().getEmail());
+		if (reservation.getUser().getEmail() == null)
+		{
+			auditLogService.reservationUserEmailInvalid(reservation);
+			return;
+		}
+		System.out.println("Sending email to " + reservation.getUser().getEmail());
 
-        sendNotificationEmail(reservation.getUser().getEmail(), title, html);
-        auditLogService.reservationCreatedEmailLog(reservation);
-    }
+		sendNotificationEmail(reservation.getUser().getEmail(), title, html);
+		auditLogService.reservationCreatedEmailLog(reservation);
+	}
 
-    //daily 06:00 AM, checks for expired notifications and mails the user + admin
-    @Scheduled(cron = "0 0 6 1/1 * *")
-    public void reservationExpiredNotification() throws MessagingException {
-        Collection<Reservation> reservations = reservationService.loadActive();
-        String title = "Reservation expired!";
-        String title_admin = "User-Reservation expired!";
+	// daily 06:00 AM, checks for expired notifications and mails the user + admin
+	@Scheduled(cron = "0 0 6 1/1 * *")
+	public void reservationExpiredNotification() throws MessagingException
+	{
+		Collection<Reservation> reservations = reservationService.loadActive();
+		String title = "Reservation expired!";
+		String title_admin = "User-Reservation expired!";
 
-        for(Reservation res: reservations){
-            String html =
-                    "<h1>Please return your reserved item!</h1><br>" +
-                            "The item: " + res.getItem().getLabItem().getItemName() +
-                            "must be returned as soon as possible." +
-                            "<br>Reservation due date: " + res.getReturnableDate() +
-                            "<br><br>Kind regards, <br>Group 4";
+		for (Reservation res : reservations)
+		{
+			String html = "<h1>Please return your reserved item!</h1><br>" + "The item: "
+					+ res.getItem().getLabItem().getItemName() + "must be returned as soon as possible."
+					+ "<br>Reservation due date: " + res.getReturnableDate() + "<br><br>Kind regards, <br>Group 4";
 
-            String html_admin =
-                    "<h1>A users reservation expired!</h1><br>" +
-                            "The item: " + res.getItem().getLabItem().getItemName() +
-                            "from user" + res.getUser().getUsername() +
-                            "with:" +
-                            "<br>Reservation due date: " + res.getReturnableDate() +
-                            "expired" +
-                            "<br><br>Kind regards, <br>you";
+			String html_admin = "<h1>A users reservation expired!</h1><br>" + "The item: "
+					+ res.getItem().getLabItem().getItemName() + "from user" + res.getUser().getUsername() + "with:"
+					+ "<br>Reservation due date: " + res.getReturnableDate() + "expired"
+					+ "<br><br>Kind regards, <br>you";
 
-            if(res.getReturnableDate().after(new Date())){
+			if (res.getReturnableDate().after(new Date()))
+			{
 
-                sendNotificationEmail(res.getUser().getEmail(), title, html);
-                sendNotificationEmail(userService.loadUser("admin").getEmail(), title_admin, html_admin);
+				sendNotificationEmail(res.getUser().getEmail(), title, html);
+				sendNotificationEmail(userService.loadUser("admin").getEmail(), title_admin, html_admin);
 
-                auditLogService.reservationExpired(res);
-            }
-        }
-    }
+				auditLogService.reservationExpired(res);
+			}
+		}
+	}
 
-    //every 4 hours, checks for expiring reservations and notifies user
-    @Scheduled(cron = "0 0 0/4 1/1 * *")
-    public void reservationExpiresSoonNotification() throws MessagingException {
-        Collection<Reservation> reservations = reservationService.loadActive();
-        String title = "Reservation expires soon!";
+	// every 4 hours, checks for expiring reservations and notifies user
+	@Scheduled(cron = "0 0 0/4 1/1 * *")
+	public void reservationExpiresSoonNotification() throws MessagingException
+	{
+		Collection<Reservation> reservations = reservationService.loadActive();
+		String title = "Reservation expires soon!";
 
-        for(Reservation res: reservations){
-            String html =
-                    "<h1>Please return your reserved items on time!</h1><br>" +
-                            "The item: " + res.getItem().getLabItem().getItemName() +
-                            "must be returned until reservation due date: " + res.getReturnableDate() +
-                            "<br><br>Kind regards, <br>Group 4";
+		for (Reservation res : reservations)
+		{
+			String html = "<h1>Please return your reserved items on time!</h1><br>" + "The item: "
+					+ res.getItem().getLabItem().getItemName() + "must be returned until reservation due date: "
+					+ res.getReturnableDate() + "<br><br>Kind regards, <br>Group 4";
 
-            //Calulate time difference between returnableDate and current time
-            Date date = new Date();
-            long diff = res.getReturnableDate().getTime() - date.getTime();
-            diff = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+			// Calulate time difference between returnableDate and current time
+			Date date = new Date();
+			long diff = res.getReturnableDate().getTime() - date.getTime();
+			diff = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 
-            if(!res.isNotified() && (diff<=1)){
+			if (!res.isNotified() && (diff <= 1))
+			{
 
-                sendNotificationEmail(res.getUser().getEmail(), title, html);
-                res.setNotified(true);
-                auditLogService.reservationExpiresSoon(res);
-            }
-        }
-    }
+				sendNotificationEmail(res.getUser().getEmail(), title, html);
+				res.setNotified(true);
+				auditLogService.reservationExpiresSoon(res);
+			}
+		}
+	}
 
-    private void sendNotificationEmail(String email, String title, String html) throws MessagingException{
-        Session session = createSession();
-        MimeMessage message = new MimeMessage(session);
-        prepareEmailMessage(message, email, title, html);
-        Transport.send(message);
-    }
+	private void sendNotificationEmail(String email, String title, String html) throws MessagingException
+	{
+		Session session = createSession();
+		MimeMessage message = new MimeMessage(session);
+		prepareEmailMessage(message, email, title, html);
+		Transport.send(message);
+	}
 
-	public void sendPassword(String pass) {
-		// TODO send password via email
-		
+	public void sendPassword(String pass, String email) throws MessagingException
+	{
+		String html = "Your password is " + pass;
+		String title = "Access Data";
+		sendNotificationEmail(email, title, html);
 	}
 }
--- src/main/java/at/qe/sepm/skeleton/ui/controllers/detail/UserDetailController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/detail/UserDetailController.java
@@ -1,15 +1,9 @@
 package at.qe.sepm.skeleton.ui.controllers.detail;
 
-import at.qe.sepm.skeleton.model.User;
-import at.qe.sepm.skeleton.services.EmailService;
-import at.qe.sepm.skeleton.services.UserService;
-import at.qe.sepm.skeleton.ui.view.UserRolesView;
-import at.qe.sepm.skeleton.utils.ErrorMessage;
-import at.qe.sepm.skeleton.utils.PasswordGenerator;
-
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
@@ -17,6 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
+
+import at.qe.sepm.skeleton.model.User;
+import at.qe.sepm.skeleton.services.EmailService;
+import at.qe.sepm.skeleton.services.UserService;
+import at.qe.sepm.skeleton.ui.view.UserRolesView;
+import at.qe.sepm.skeleton.utils.ErrorMessage;
+import at.qe.sepm.skeleton.utils.PasswordGenerator;
 
 /**
  * Controller for the user detail view.
@@ -27,147 +28,161 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Scope("view")
-public class UserDetailController implements Serializable {
+public class UserDetailController implements Serializable
+{
 
 	private static final long serialVersionUID = 1377920198925965329L;
 
 	@Autowired
-    private UserService userService;
-    
-    @Autowired
-    private UserRolesView userRoleView;
-    
-    @Autowired
-    private ErrorMessage errorMessage;
-    
-    @Autowired
-    private EmailService emailService;
+	private UserService userService;
 
-    /**
-     * Attribute to cache the currently displayed user
-     */
-    private User user;
-    private User newUser;
-    
+	@Autowired
+	private UserRolesView userRoleView;
 
-    @PostConstruct
-    private void init() {
-    	newUser = new User();
-    }
+	@Autowired
+	private ErrorMessage errorMessage;
 
-    /**
-     * Sets the currently displayed user and reloads it form db. This user is
-     * targeted by any further calls of
-     * {@link #doReloadUser()}, {@link #doSaveUser()} and
-     * {@link #doDeleteUser()}.
-     *
-     * @param user
-     */
-    public void setUser(User user) {
-        this.user = user;
-        doReloadUser();
-    }
+	@Autowired
+	private EmailService emailService;
 
-    /**
-     * Returns the currently displayed user.
-     *
-     * @return
-     */
-    public User getUser() {
-        return user;
-    }
+	/**
+	 * Attribute to cache the currently displayed user
+	 */
+	private User user;
+	private User newUser;
 
-    /**
-     * Returns the new user.
-     *
-     * @return
-     */
-    public User getNewUser() {
+	@PostConstruct
+	private void init()
+	{
+		newUser = new User();
+	}
+
+	/**
+	 * Sets the currently displayed user and reloads it form db. This user is
+	 * targeted by any further calls of {@link #doReloadUser()},
+	 * {@link #doSaveUser()} and {@link #doDeleteUser()}.
+	 *
+	 * @param user
+	 */
+	public void setUser(User user)
+	{
+		this.user = user;
+		doReloadUser();
+	}
+
+	/**
+	 * Returns the currently displayed user.
+	 *
+	 * @return
+	 */
+	public User getUser()
+	{
+		return user;
+	}
+
+	/**
+	 * Returns the new user.
+	 *
+	 * @return
+	 */
+	public User getNewUser()
+	{
 		return newUser;
 	}
 
-    /**
-     * Sets the new user.
-     *
-     * @param user
-     */
-	public void setNewUser(User newUser) {
+	/**
+	 * Sets the new user.
+	 *
+	 * @param user
+	 */
+	public void setNewUser(User newUser)
+	{
 		this.newUser = newUser;
 	}
 
 	/**
-     * Action to force a reload of the currently displayed user.
-     */
-    public void doReloadUser() {
-        user = userService.loadUser(user.getUsername());
-    }
+	 * Action to force a reload of the currently displayed user.
+	 */
+	public void doReloadUser()
+	{
+		user = userService.loadUser(user.getUsername());
+	}
 
-    /**
-     * Action to save the currently displayed user.
-     */
-    public void doSaveUser() {
-        user = this.userService.saveUser(user);
-    }
+	/**
+	 * Action to save the currently displayed user.
+	 */
+	public void doSaveUser()
+	{
+		user = this.userService.saveUser(user);
+	}
 
-    /**
-     * Action to delete the currently displayed user.
-     */
-    public void doDeleteUser() {
-        this.userService.deleteUser(user);
-        user = null;
-    }
-   
-    /**
-     * Action to add a new user.
-     */
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public void doAddUser() {
-    	
-    	if (newUser.getUsername().length() <= 3) {
-    		errorMessage.pushMessage("Username not valid");
-    		return;
-    	}
-    	
-    	
-    	if (newUser.getFirstName() == "") {
-    		errorMessage.pushMessage("Firstname not valid");
-    		return;
-    	}
-    	
-    	if (newUser.getLastName() == "") {
-    		errorMessage.pushMessage("Lastname not valid");
-    		return;
-    	}
-    	
-    	String email = newUser.getEmail();    	
-		try {
+	/**
+	 * Action to delete the currently displayed user.
+	 */
+	public void doDeleteUser()
+	{
+		this.userService.deleteUser(user);
+		user = null;
+	}
+
+	/**
+	 * Action to add a new user.
+	 * 
+	 * @throws MessagingException
+	 */
+	@PreAuthorize("hasAuthority('ADMIN')")
+	public void doAddUser() throws MessagingException
+	{
+
+		if (newUser.getUsername().length() <= 3)
+		{
+			errorMessage.pushMessage("Username not valid");
+			return;
+		}
+
+		if (newUser.getFirstName() == "")
+		{
+			errorMessage.pushMessage("Firstname not valid");
+			return;
+		}
+
+		if (newUser.getLastName() == "")
+		{
+			errorMessage.pushMessage("Lastname not valid");
+			return;
+		}
+
+		String email = newUser.getEmail();
+		try
+		{
 			InternetAddress emailAddr = new InternetAddress(email);
 			emailAddr.validate();
-		} catch (AddressException ex) {
+		} catch (AddressException ex)
+		{
 			errorMessage.pushMessage("Email not validl!");
 			return;
 		}
-    	
+
 		newUser.setRoles(userRoleView.getSelectedSet());
-    	if (newUser.getRoles() == null) {
-    		errorMessage.pushMessage("No user authorization selected");
-    		return;
-    	}
-    
-    	
-    	String pass = PasswordGenerator.getRandomPassword(5);
-    	String hashedPass = PasswordGenerator.hashPassword(pass);
-    	
-    	newUser.setPassword(hashedPass);
-    	newUser.setEnabled(true);
-    	
-    	
-    	if (userService.saveUser(newUser) != null) {
-    		emailService.sendPassword(pass);
-    	}
-    	
-    	newUser = new User();
-    	errorMessage.reset();
-    }
+		if (newUser.getRoles() == null)
+		{
+			errorMessage.pushMessage("No user authorization selected");
+			return;
+		}
+
+		String pass = PasswordGenerator.getRandomPassword(5);
+		String hashedPass = PasswordGenerator.hashPassword(pass);
+
+		newUser.setPassword(hashedPass);
+		newUser.setEnabled(true);
+
+		if (userService.saveUser(newUser) != null)
+		{
+			emailService.sendPassword(pass, newUser.getEmail());
+		}
+
+		newUser = new User();
+		errorMessage.reset();
+	}
 
 }
